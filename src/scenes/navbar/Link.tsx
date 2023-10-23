b2826285312d57b2b99fb7eace071f2a8d088060
RomaSamkov <Romansamkov.rs@gmail.com>
2023-10-23 16:40:52 +0300

    Update Link.tsx

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,13 +1,14 @@
+import { SelectedPage } from "@/shared/types";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   page: string;
-  selectedPage: string;
-  setSelectedPage: (value: string) => void;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
 function Link({ page, selectedPage, setSelectedPage }: Props) {
-  const lowerCasePAge = page.toLowerCase().replace(/ /g, "");
+  const lowerCasePAge = page.toLowerCase().replace(/ /g, "") as SelectedPage;
   return (
     <AnchorLink
       className={`${
